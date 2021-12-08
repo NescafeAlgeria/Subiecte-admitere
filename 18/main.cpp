702d1703eac1d18a7b48b27157628741fb1aaae9
NescafeAlgeria <49836053+NescafeAlgeria@users.noreply.github.com>
2021-12-08 09:12:46 +0200

    Create main.cpp

---
+++ 18/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("matrice.in");
+
+int main(){
+    int n;
+    int s=0;
+    fin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            int a;
+            fin>>a;
+            if(i+j>n+1){
+                s+=a;
+            }
+        }
+    }
+    cout<<s;
+    return 0;
+}
